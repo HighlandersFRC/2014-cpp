f64c430aa2373d49c09631113e8ebdfe32061ccd
Drago901 <dragodrew@gmail.com>
2013-09-20 20:28:05 -0700

    Drew Subsystem and Command added

---
+++ CollaborationTest/Commands/DrewCommand.h
@@ -0,0 +1,22 @@
+#ifndef DREWCOMMAND_H
+#define DREWCOMMAND_H
+
+#include "Commands/Subsystem.h"
+#include "../Robot.h"
+
+/**
+ *
+ *
+ * @author Drew
+ */
+class DrewCommand: public Command {
+public:
+	DrewCommand();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
---
+++ CollaborationTest/Subsystems/DrewSubsystem.h
@@ -0,0 +1,20 @@
+#ifndef DREWSUBSYSTEM_H
+#define DREWSUBSYSTEM_H
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+/**
+ *
+ *
+ * @author Drew
+ */
+class DrewSubsystem: public Subsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+public:
+	DrewSubsystem();
+	void InitDefaultCommand();
+};
+
+#endif
