1c25c6a03763198302802a5a87d70e52d882d11d
aiverson <alexjiverson@gmail.com>
2013-09-27 19:58:37 -0600

    ported the drive base subsystem

--- OldRobot/Subsystems/DriveBase.cpp
+++ OldRobot/Subsystems/DriveBase.cpp
@@ -34,4 +34,6 @@ void DriveBase::InitDefaultCommand() {
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
-
+void DriveBase::drive(double x, double y, double theta) {
+	robotDrive41->MecanumDrive_Cartesian(x, y, theta, 0);
+}
