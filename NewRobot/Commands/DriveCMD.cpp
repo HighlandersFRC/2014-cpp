92ddc2e4e3d77312f3d2899c858dbf0fdaaf7712
Zack Palmer <zacke.palmer@gmail.com>
2014-01-07 22:48:20 -0700

    Removed redundant code
    Logic Changes -
    Changed joysticks in OI from private to public in order to remove get
    joysticks functions that are redundant.
    
    Other Changes -
    None

--- NewRobot/Commands/DriveCMD.cpp
+++ NewRobot/Commands/DriveCMD.cpp
@@ -15,10 +15,10 @@ void DriveCMD::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void DriveCMD::Execute() {
 	// Get Joystick axis values
-	double x_left  = oi->GetJoystick1()->GetX(); // left joystick x-axis
-	double y_left  = oi->GetJoystick1()->GetY(); // left joystick y-axis
-	double x_right = oi->GetJoystick2()->GetX(); // right joystick x-axis
-	double y_right = oi->GetJoystick2()->GetY(); // right joystick y-axis
+	double x_left  = oi->joystick_1->GetX(); // left joystick x-axis
+	double y_left  = oi->joystick_1->GetY(); // left joystick y-axis
+	double x_right = oi->joystick_2->GetX(); // right joystick x-axis
+	double y_right = oi->joystick_2->GetY(); // right joystick y-axis
 	
 	drivetrain->TankDrive(y_left, y_right);      // Tank Drive control style (two joysticks)
 	//drivetrain->ArcadeDrive(x_left, y_left);     //Arcade Drive control style (one joystick)
--- NewRobot/OI.h
+++ NewRobot/OI.h
@@ -4,17 +4,12 @@
 #include "WPILib.h"
 
 class OI {
-	private:
-		// Controls
-		Joystick *joystick_1;
-		Joystick *joystick_2;
-		
 	public:
 		OI();
 		
-		// Control Access Functions
-		Joystick* GetJoystick1();
-		Joystick* GetJoystick2();
+		// Controls
+		Joystick *joystick_1;
+		Joystick *joystick_2;
 };
 
 #endif
