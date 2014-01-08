7dd3473d9253cc973f2795afcc7f3da828b49825
Zack Palmer <zacke.palmer@gmail.com>
2014-01-07 22:27:34 -0700

    Added Comments to OI.cpp
    Logic Changes -
    None.
    
    Other Changes -
    Added comments to OI.cpp

--- NewRobot/OI.cpp
+++ NewRobot/OI.cpp
@@ -1,14 +1,43 @@
 #include "OI.h"
 
+
+/* OI::OI()
+ * Inputs  - 
+ * 		None
+ * Returns - 
+ * 		None
+ * 
+ * Creates control objects and assigns
+ * commands to events.
+*/
 OI::OI() {
 	// Process operator interface input here.
 	joystick_1 = new Joystick(1);
 	joystick_2 = new Joystick(2);
 }
+
+
+/* OI::GetJoystick1()
+ * Inputs  - 
+ * 		None
+ * Returns - 
+ * 		Joystick*
+ * 
+ * Returns Joystick 1 pointer
+*/
 Joystick* OI::GetJoystick1() {
 	return joystick_1;
 }
+
+
+/* OI::GetJoystick2()
+ * Inputs  - 
+ * 		None
+ * Returns - 
+ * 		Joystick*
+ * 
+ * Returns Joystick 2 pointer
+*/
 Joystick* OI::GetJoystick2() {
 	return joystick_2;
 }
-
