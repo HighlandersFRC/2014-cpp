801378e9d592d333908da51269a5d0a9b9474ed0
zack-arduino <zacke-palmer@gmail.com>
2014-01-19 19:38:59 -0700

    Updated Robotmap.h with intake and user names

--- NewRobot/Robotmap.h
+++ NewRobot/Robotmap.h
@@ -10,24 +10,25 @@
  * floating around.
 */
 
-//      Name                   Number      Bus               Description                    Subsystem
-#define LEFT_DRIVE_MOTOR_1     1           // PWM Out        Left drive motor port 1        chassis
-#define LEFT_DRIVE_MOTOR_2     2           // PWM Out        Left drive motor port 2        chassis
-#define RIGHT_DRIVE_MOTOR_1    3           // PWM Out        Right drive motor port 1       chassis
-#define RIGHT_DRIVE_MOTOR_2    4           // PWM Out        Right drive motor port 2       chassis
-#define LEFT_ENCODER_A         1           // I/O            Left Encoder data A port       chassis
-#define LEFT_ENCODER_B         2           // I/O            Left Encoder data B port       chassis
-#define DIST_SENS              2           // Analog in      Front Distance Sensor          chassis
-#define GYRO_SENS              1           // Analog in      gyro for dirrection            chassis
-#define SHIFTER_A              1           // Pneumatic      forward gear shifter           chassis
-#define SHIFTER_B              2           // Pneumatic      reverse gear shifter           chassis
-#define COMPRESSOR_RELAY       1           // Relay          compressor motor relay         chassis
-#define COMPRESSOR_SWITCH      3           // I/O            compressor limit switch        chassis
-
-#define KICKER_MOTOR_1         5           // PWM Out        Kicker Motor 1                 kicker
-#define KICKER_MOTOR_2         6           // PWM Out        Kicker Motor 2                 kicker
-
-#define INTAKE_MOTOR		   7		   // PWM Out		 Intake Motor					intake
-
+//      Name                   Number      Bus               Description                      Subsystem        User
+#define LEFT_DRIVE_MOTOR_1     1           // PWM Out        Left drive motor port 1          chassis          Zack
+#define LEFT_DRIVE_MOTOR_2     2           // PWM Out        Left drive motor port 2          chassis          Zack
+#define RIGHT_DRIVE_MOTOR_1    3           // PWM Out        Right drive motor port 1         chassis          Zack
+#define RIGHT_DRIVE_MOTOR_2    4           // PWM Out        Right drive motor port 2         chassis          Zack
+#define LEFT_ENCODER_A         1           // I/O            Left Encoder data A port         chassis          Zack
+#define LEFT_ENCODER_B         2           // I/O            Left Encoder data B port         chassis          Zack
+#define DIST_SENS              2           // Analog in      Front Distance Sensor            chassis          Zack
+#define GYRO_SENS              1           // Analog in      gyro for direction               chassis          Linda
+#define SHIFTER_A              1           // Pneumatic      forward gear shifter             chassis          Zack
+#define SHIFTER_B              2           // Pneumatic      reverse gear shifter             chassis          Zack
+#define COMPRESSOR_RELAY       1           // Relay          compressor motor relay           compressor       Drew
+#define COMPRESSOR_SWITCH      3           // I/O            compressor limit switch          compressor       Drew
+                                                                                                               
+#define KICKER_MOTOR_1         5           // PWM Out        Kicker Motor 1                   kicker           Linda
+#define KICKER_MOTOR_2         6           // PWM Out        Kicker Motor 2                   kicker           Linda
+                                                                                                               
+#define BELT_MOTOR             8           // PWM Out        elevation control for platform   platform         Zack
+                                                                                                               
+#define INTAKE_MOTOR           7           // PWM Out        ball grab motor                  Intake           Drew
 
 #endif
