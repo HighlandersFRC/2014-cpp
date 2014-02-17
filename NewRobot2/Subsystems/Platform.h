10cbea908ae763c42fbe8ac713085f58de193b29
javario <javario.the.hero@gmail.com>
2014-02-17 10:34:35 -0700

    changed elevator files to Platform files.

---
+++ NewRobot2/Subsystems/Platform.h
@@ -0,0 +1,39 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#include "Commands/PIDSubsystem.h"
+#include "WPILib.h"
+
+/**
+ *
+ *
+ * @author Javario
+ */
+class Platform: public PIDSubsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	// set the P, I, and D constants here
+		Jaguar *Belt_Motor;
+		AnalogChannel *Belt_Pot;
+		PIDController *pid;
+	static const double STOW = 1.65;
+	static const double Kp = 0.1;
+	static const double Ki = 0.0;
+	static const double Kd = 0.0;
+	double curheight;
+	double posspeed;
+	double voltage;
+	double storeheight;
+public:
+	Platform();
+	double ReturnPIDInput();
+	void UsePIDOutput(double output);
+	void InitDefaultCommand();
+	void setSpeed(double speed);
+	double getBeltPot();
+	void setPosition(double position);
+	void requestPosition(double wantedheight);
+};
+
+#endif
